a9d333cf935a27280bb4a619599ec131fac00b73
SowjanyaRao Yadavalli <sowjanyarao.yadavalli@thomsonreuters.com>
2020-05-24 19:06:09 +0530

    sample project - initial commit

---
+++ names.js
@@ -0,0 +1,41 @@
+var data = {
+    "1_1": [{
+        "name": "Insurance 1",
+        "price": 100,
+		"id": "insurance_1_col"
+    },{
+        "name": "Insurance 2",
+        "price": 200,
+		"id": "insurance_2_col"
+    },{
+        "name": "Insurance 3",
+        "price": 300,
+		"id": "insurance_3_col"
+    }],
+    "1_2": [{
+        "name": "Insurance 1",
+        "price": 100,
+		"id": "insurance_1_col"
+    },{
+        "name": "Insurance 3",
+        "price": 300,
+		"id": "insurance_3_col"
+    },{
+        "name": "Insurance 2",
+        "price": 200,
+		"id": "insurance_2_col"
+    }],
+    "2_1": [{
+        "name": "Insurance 3",
+        "price": 300,
+		"id": "insurance_3_col"
+    },{
+        "name": "Insurance 2",
+        "price": 200,
+		"id": "insurance_2_col"
+    },{
+        "name": "Insurance 1",
+        "price": 100,
+		"id": "insurance_1_col"
+    }],
+};
